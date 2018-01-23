2c3361724c4b466cc8f58ec2f7d6772f16eed1d3
Elisa Martinez <97morningstar@gmail.com>
2018-01-23 14:23:27 -0600

    change database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        	User::truncate();
+
+        	$user = User::create([
+        		'name' => 'Elisa',
+        		'email' => '97mornigstar@gmail.com',
+        		'password' => 'elisa1997'
+        	]);
     }
 }
